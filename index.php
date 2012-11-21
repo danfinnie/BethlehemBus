4382b553487865c3b65b5542329f111f2c8f9ff8
Dan Finnie <dfinnie@noisenewyork.com>
2012-11-20 21:47:00 -0500

    Code to sort departure times by next availablity

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+class TimeOfDay {
+	private $hour;
+	private $min;
+	private $ampm;
+
+	function __construct($str) {
+		// var_dump($str);
+		$matches = null;
+		preg_match('/^(\d\d?)(\d\d)(a|p)$/', $str, $matches);
+		$this->hour = intval($matches[1]);
+		$this->min = intval($matches[2]);
+		$this->ampm = $matches[3] . "m";
+	}
+
+	// Returns true if the next instance of this TimeOfDay occurs before $other.
+	function compareNextOccurence($other) {
+		$me = $this->getNextOccurence();
+		$friend = $other->getNextOccurence();
+
+		if ($me < $friend) {
+			return -1;
+		} else if ($me === $friend) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+
+	private function getNextOccurence() {
+		$today = strtotime($this->getHumanReadable());
+
+		if($today < time()) {
+			return strtotime("+1 day", $today);
+		} else {
+			return $today;
+		}
+	}
+
+	private function getHumanReadable() {
+		return sprintf('%d:%02d%s', $this->hour, $this->min, $this->ampm);
+	}
+}
+
+$data = json_decode(file_get_contents("data.json"), true);
+$data = $data['westbound'];
+
+usort($data, function($a, $b) {
+	$a = new TimeOfDay($a['departureTime']);
+	$b = new TimeOfDay($b['departureTime']);
+
+	return $a->compareNextOccurence($b);
+});
+
+
+
+
+?><!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>

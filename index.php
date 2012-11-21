d8751ac94202a0c6e93e941ef3c66ae5116fc729
Dan Finnie <dfinnie@noisenewyork.com>
2012-11-20 23:52:56 -0500

    Add title to web page

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ for ($i = 0; $i < 2; $i++) {
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Bethlehem Buses</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
